80058ce0779fdcecc6bc177c5c27be4827aca96f
tony0831-L <t0983822437@gmail.com>
2021-10-23 04:16:15 +0800

    section

---
+++ index.js
@@ -0,0 +1,49 @@
+Vue.createApp({
+    data() {
+        return {
+            now:"全部展示",
+            data: [
+                {
+                    catgolery:"課堂實作",
+                    info:[
+                        {
+                            title:"Introduction",
+                            subtitle:"W02 - 課堂實作",
+                            img:'./img/3.png',
+                            href:"https://tony0831-l.github.io/1101-CLASSDEMO-409631024/w02/introduction.html"
+                        },
+                        {
+                            title:"Pokemon",
+                            subtitle:"W02 - 課堂實作",
+                            img:'./img/2.png',
+                            href:"https://tony0831-l.github.io/1101-CLASSDEMO-409631024/w02/poketmon.html"
+                        },
+                        {
+                            title:"Image Gallery",
+                            subtitle:"W04 - 課堂實作",
+                            img:'./img/1.png',
+                            href:"https://tony0831-l.github.io/1101-CLASSDEMO-409631024/w04/ImageGallery/ImageGallery.html"
+                        },
+                        {
+                            title:"Blog",
+                            subtitle:"W04 - 課堂實作",
+                            img:'./img/4.png',
+                            href:"https://tony0831-l.github.io/1101-CLASSDEMO-409631024/w04/blog/blog.html"
+                        },
+                    ],
+                },
+                {
+                    catgolery:"自主學習",
+                    info:[
+                        {
+                            title:"My photo gallery",
+                            subtitle:"W04 - 自主學習",
+                            img:'./w04/imgs/Web 1920 – 2.png',
+                            href:"https://tony0831-l.github.io/1101-CLASSDEMO-409631024/w04/myPhotoGallery/myPhotoGallery.html"
+                        },
+                    ],
+                }
+            ]
+        }
+    }
+}).mount('#app')
--- w04/myPhotoGallery/README.md
+++ w04/myPhotoGallery/README.md
@@ -17,30 +17,3 @@
 
 ![](https://i.imgur.com/EzMDbqw.png)
 
-
-
-
-
-
-
-
-
-# 3. 心得甘苦談
-
-### 這次的所有項目都是手刻的,全體都有rwd設計,比較有挑戰性的是要完全不依賴第三方ui框架或是Vuejs等框架,雖然難倒是沒有甚麼難度,但要在短時間內完全一個人發想文案,畫出設計稿,手動切版等等,這還真的是不太容易。
-
-
-
-### 使用工具類:ClipStudioPaint(插畫,圖片編輯), AdobeXD(線框稿設計與排版), Zeplin(線框稿導出為設計規範圖), VsCode(editor)。
-
-
-
-## 下列為項目截圖
-
-## 電子履歷
-
-![](https://i.imgur.com/uluWaxK.png)
-
-## 英文與多圖排版
-
-![](https://i.imgur.com/mAHTYJu.png)
